4e7c3ad45bb484f138071ec602e46b5a6dfdc193
MirjahonMirsaidov <mirjahonac021@gmail.com>
2024-08-17 14:40:41 +0500

    deploy

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -23,25 +23,25 @@ sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 # Enable Docker service to start on boot
 sudo systemctl enable docker
-
-# Create a directory for the project
-mkdir -p /home/ec2-user/myapp
-cd /home/ec2-user/myapp
-
-# Create the Docker Compose file
-cat <<EOF > docker-compose.yml
-version: '3.9'
-
-services:
-  web:
-    image: public.ecr.aws/a8p1y2a7/wr:latest
-    platform: linux/amd64
-    command: python manage.py runserver 0.0.0.0:8000
-    ports:
-      - "80:8000"
-    environment:
-      - DEBUG=True
-EOF
+#
+## Create a directory for the project
+#mkdir -p /home/ec2-user/myapp
+#cd /home/ec2-user/myapp
+#
+## Create the Docker Compose file
+#cat <<EOF > docker-compose.yml
+#version: '3.9'
+#
+#services:
+#  web:
+#    image: public.ecr.aws/a8p1y2a7/wr:latest
+#    platform: linux/amd64
+#    command: python manage.py runserver 0.0.0.0:8000
+#    ports:
+#      - "80:8000"
+#    environment:
+#      - DEBUG=True
+#EOF
 
 
 
@@ -67,11 +67,11 @@ sudo cat <<EOF > config.json
 }
 EOF
 # Start CloudWatch agent
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ec2-user/config.json -s 
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ec2-user/config.json -s
 
 
-# Change ownership of the directory to ec2-user
-chown -R ec2-user:ec2-user /home/ec2-user/myapp
-
-# Run the Docker Compose up command
-sudo -u ec2-user docker-compose up -d
+## Change ownership of the directory to ec2-user
+#chown -R ec2-user:ec2-user /home/ec2-user/myapp
+#
+## Run the Docker Compose up command
+#sudo -u ec2-user docker-compose up -d
