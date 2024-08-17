7c45fc7e1a2bab5d15f67fdc3eb5415e4ed7404e
MirjahonMirsaidov <mirjahonac021@gmail.com>
2024-08-17 16:34:42 +0500

    deploy

--- terraform/cache.tf
+++ terraform/cache.tf
@@ -1,3 +1,24 @@
+# Create Security Groups
+# Allow EC2 sg on Redis
+resource "aws_security_group" "elasticache" {
+  name        = "elasticache"
+  description = "Allow EC2 access on Redis"
+  vpc_id      = aws_vpc.prod_vpc.id
+
+  tags = {
+    Name = "elasticache"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ec2_on_redis" {
+  security_group_id = aws_security_group.elasticache.id
+  referenced_security_group_id = aws_security_group.allow_web_on_ec2.id
+  from_port         = 6379
+  ip_protocol       = "tcp"
+  to_port           = 6379
+}
+
+
 # Create Subnet group
 resource "aws_elasticache_subnet_group" "prod" {
   name       = "prod"
@@ -21,4 +42,5 @@ resource "aws_elasticache_cluster" "prod_redis" {
   engine_version       = "7.1"
   port                 = 6379
   subnet_group_name = aws_elasticache_subnet_group.prod.name
+  security_group_ids = [aws_security_group.elasticache.id]
 }
